136f9905617ab7f268d4b7ce2067a2a62da1f88f
Camden Landis <craine@vt.edu>
2022-06-24 23:55:57 -0400

    Updated header file.

--- matrix/matrix.h
+++ matrix/matrix.h
@@ -7,5 +7,12 @@ typedef struct {
 } Matrix;
 
 Matrix* matrix_create(int row, int col);
-void matrix_fill(Matrix *m, int n);
-void matrix_free(Matrix *m);
+void matrix_fill(Matrix *A, int n);
+void matrix_free(Matrix *A);
+void matrix_print(Matrix *A);
+Matrix* matrix_copy(Matrix *A);
+void matrix_file_save(Matrix *A, char* filename);
+Matrix* matrix_load(char* filename);
+void random_matrix(Matrix *A, int n);
+int matrix_argmax(Matrix *A);
+Matrix* matrix_flatten(Matrix *A, int dim);
